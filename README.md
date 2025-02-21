264504be280994a22414d9cb1f46cc9f0b43a548
gilberto-domingos <domingoshot@hotmail.com>
2025-02-21 02:15:32 -0300

    commit init (#43670)

---
+++ README.md
@@ -0,0 +1,1 @@
+# python_rabbitMQ_messaging
---
+++ consumer_raw.py
@@ -0,0 +1,31 @@
+import pika
+
+
+def my_callback(ch, method, properties, body):
+    print(body)
+    print(type(body))
+
+
+connection_parameters = pika.ConnectionParameters(
+    host="localhost",
+    port=5672,
+    credentials=pika.PlainCredentials(
+        username="admin",
+        password="senha_segura"
+    )
+)
+
+channel = pika.BlockingConnection(connection_parameters).channel()
+channel.queue_declare(
+    queue="data_queue",
+    durable=True
+)
+
+channel.basic_consume(
+    queue="data_queue",
+    auto_ack=True,
+    on_message_callback=my_callback
+)
+
+print(f'Listen RabbitMQ on Port 5672')
+channel.start_consuming()
