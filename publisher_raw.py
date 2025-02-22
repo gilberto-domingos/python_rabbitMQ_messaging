ea4f2304cd7853c50670f7f5579d21a06c50ef84
gilberto-domingos <domingoshot@hotmail.com>
2025-02-21 23:44:38 -0300

    implementing publisher raw (#43672)

---
+++ publisher_raw.py
@@ -0,0 +1,21 @@
+import pika
+
+
+connection_parameters = pika.ConnectionParameters(
+    host="localhost",
+    port=5672,
+    credentials=pika.PlainCredentials(
+        username="admin",
+        password="senha_segura"
+    )
+)
+
+channel = pika.BlockingConnection(connection_parameters).channel()
+channel.basic_publish(
+    exchange="data_exchange",
+    routing_key="",
+    body="sending my message",
+    properties=pika.BasicProperties(
+        delivery_mode=2
+    )
+)
