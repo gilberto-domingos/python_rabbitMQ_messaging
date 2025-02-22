08360dbdc3e445d5b96f917873770ff20145748a
gilberto-domingos <domingoshot@hotmail.com>
2025-02-22 11:03:09 -0300

    refatorate publisher_raw.py in publisher.py to POO (#43673)

---
+++ publisher.py
@@ -0,0 +1,41 @@
+from typing import Dict
+import pika
+import json
+
+
+class RabbitMqPublisher:
+    def __init__(self) -> None:
+        self.__host = "localhost"
+        self.__port = 5672
+        self.__username = "admin"
+        self.__password = "senha_segura"
+        self.__exchange = "data_exchange"
+        self.__routing_key = ""
+        self.__channel = self.__create_channel()
+
+    def __create_channel(self):
+        connection_parameters = pika.ConnectionParameters(
+            host=self.__host,
+            port=self.__port,
+            credentials=pika.PlainCredentials(
+                username=self.__username,
+                password=self.__password
+            )
+        )
+
+        channel = pika.BlockingConnection(connection_parameters).channel()
+        return channel
+
+    def send_message(self, body: Dict):
+        self.__channel.basic_publish(
+            exchange=self.__exchange,
+            routing_key=self.__routing_key,
+            body=json.dumps(body),
+            properties=pika.BasicProperties(
+                delivery_mode=2
+            )
+        )
+
+
+rabbitMqPublisher = RabbitMqPublisher()
+rabbitMqPublisher.send_message({"Hello": "World !"})
